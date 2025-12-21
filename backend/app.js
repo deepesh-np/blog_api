861b85ad3c5bdfc18b3d65acde735b2a11f72f08
deepesh-np <nrndpsh@gmail.com>
2025-12-21 23:32:23 +0530

    init

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDB } from "./config/db.js";
+connectDB();
+
+const app = express();
+const port = process.env.port;
+
+app.get('/',(req,res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ backend/models/articleModel.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import { Schema } from 'mongoose';
+
+articleSchema = new mongoose.Schema(
+    
+)
