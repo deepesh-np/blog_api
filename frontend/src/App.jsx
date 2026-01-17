2c3ee3aab6ee01730a4f8925d0223ba3443cabf1
deepesh-np <nrndpsh@gmail.com>
2026-01-17 20:10:30 +0530

    feat: remove Redis dependencies and enhance theme support in CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,48 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar.jsx";
-import Home from "./Pages/Home.jsx";
-import Login from "./Pages/Login.jsx";
-import Register from "./Pages/Register.jsx";
-import Profile from "./Pages/Profile.jsx";
-import Publish from "./Pages/Publish.jsx";
-import Article from "./Pages/Article.jsx";
-import NotFound from "./Pages/NotFound.jsx";
+/** @format */
 
-import ProtectedRoute from "./Components/ProtectedRoute.jsx"
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar.jsx';
+import Home from './Pages/Home.jsx';
+import Login from './Pages/Login.jsx';
+import Register from './Pages/Register.jsx';
+import Profile from './Pages/Profile.jsx';
+import Publish from './Pages/Publish.jsx';
+import Article from './Pages/Article.jsx';
+import NotFound from './Pages/NotFound.jsx';
+
+import ProtectedRoute from './Components/ProtectedRoute.jsx';
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-bg text-text min-h-screen transition-colors duration-200'>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/article/:slug" element={<Article />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/logout' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/article/:slug' element={<Article />} />
 
         <Route
-        path="/publish"
-        element={
-          <ProtectedRoute>
-            <Publish />
-          </ProtectedRoute>
-        }
-      />
+          path='/publish'
+          element={
+            <ProtectedRoute>
+              <Publish />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
 
-        <Route path="*" element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   );
