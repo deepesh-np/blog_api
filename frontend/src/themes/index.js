f57dcb1c73ff02db4ab456cecda53fd9de97c73d
deepesh-np <nrndpsh@gmail.com>
2026-01-17 20:09:09 +0530

    feat: implement theme provider and toggle functionality with light/dark mode support

---
+++ frontend/src/themes/index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+// export { ThemeProvider } from './ThemeProvider.jsx';
+// export { useTheme } from './useTheme.js';
+// import { themes } from './theme.js';
+
+export { ThemeProvider } from './ThemeProvider.jsx';
+export { useTheme } from './useTheme.js';
+export { themes } from './theme.js';
