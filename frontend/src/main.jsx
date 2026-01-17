2e729dbf40b253ad8424e45735e9bc05f548a060
deepesh-np <nrndpsh@gmail.com>
2026-01-17 20:10:53 +0530

    theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,22 @@
-import { StrictMode } from 'react'
+/** @format */
+
+import { StrictMode } from 'react';
 import React from 'react';
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {BrowserRouter} from "react-router-dom";
-import App from './App.jsx'
-import { AuthProvider } from "./context/AuthProvider";
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { AuthProvider } from './context/AuthProvider';
+import { ThemeProvider } from './themes/ThemeProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-</AuthProvider>
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
